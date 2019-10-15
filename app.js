b39a7d9bff6b276dae7f25c7d48eee0a61a4657d
tumiMsaki <245551699@qq.com>
2019-10-15 12:49:12 +0800

    fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { set, get } = require('./src/db/redis')
 
 const getCookieExpires = () => {
   const d = new Date()
-  d.setTime(d.getTime() + (1000))
+  d.setTime(d.getTime() + (24 * 60 * 60))
   return d.toUTCString()
 }
 
