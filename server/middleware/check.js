8805a1b3ff47dbc08df8403faae6cd9f39a2428f
tumiMsaki <245551699@qq.com>
2019-10-28 16:54:50 +0800

    check login

--- server/middleware/check.js
+++ server/middleware/check.js
@@ -1,11 +1,4 @@
 module.exports ={
-  checkNotLogin: (ctx) => {
-    if (ctx.session && ctx.session.user) {     
-      ctx.redirect('/posts');
-      return false;
-    }
-    return true;
-  },
   checkLogin: (ctx) => {
     if (!ctx.session || !ctx.session.user) {     
       ctx.redirect('api/Signin');
