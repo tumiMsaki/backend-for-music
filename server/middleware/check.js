c9c7bceb7ff6f806a30be3bb76ffcfb22eda212b
tumiMsaki <245551699@qq.com>
2019-10-29 23:12:14 +0800

    init

--- server/middleware/check.js
+++ server/middleware/check.js
@@ -1,16 +1,26 @@
+import redis from '../../init/util/redis'
 module.exports = () => {
   return async (ctx, next) => {
     if (ctx.request.url === '/api/login' || ctx.request.url === '/api/regist') {
       return await next()
     }
+
     const name = ctx.session.name
     if (!name) {
       return ctx.body = {
-        code: 4,
+        err_code: 4,
         msg: '未登陆或登陆过期'
       }
-    } else {
-      return await next()
     }
+
+    const redisData = await redis.get(name);
+    if (!redisData) {
+      return ctx.body = {
+        err_code: 1,
+        msg: 'SESSIONID已经过期~',
+      }
+    }
+    
+    return await next()
   }
 }
